1cb88488fd2fde9089efb2ab3814dd76c1fedce2
AbhilashaGujarathi <abhsatle@in.ibm.com>
2024-05-31 21:02:35 +0530

    Add files via upload

---
+++ SQL/Activities/Activity9.sql
@@ -0,0 +1,45 @@
+Skill 
+
+Banking Industry Jump Start
+Automation with Selenium 
+On going Learning Perfomance Testing with JMeter through - IBM Udemy 
+
+**Kindly join for the evaluation based on your respective time slots mentioned
+
+ 
+
+Common Interview Link: https://teams.microsoft.com/l/meetup-join/19%3ameeting_NTllOWFiNmQtZDIwMy00MGMwLWE4YTctY2QxYWI2NWM5YmI5%40thread.v2/0?context=%7b%22Tid%22%3a%22404b1967-6507-45ab-8a6d-7374a3f478be%22%2c%22Oid%22%3a%2209ad579a-52d2-4c46-8d86-fcc8975c1d2e%22%7d
+
+Responsibility to Others
+
+Team Player Award received by Client
+ 
+1. Knowledge Sharing activities within the Project 
+2. Effective teaming  , participation in teaming activities
+3. Effective communication within the team 
+Help out new and junior members of the team as appropriate in terms of mentoring and guidance around technical/functional area.  Effective execution of Knowledge Transfer to New members (induction) (KT as part of job transition not considered)
+Provide SME support across Project
+
+
+Innovation
+
+Contributions in process & productivity improvements
+Development /Usage of assets/accelerators
+
+
+Client Success  
+
+No delivery related Customer escalations 
+Deliver Value add to the customer  - In the form of usage of new solutions,  assets/accelerators
+Effective responsiveness to Customer requests
+
+Business Results
+
+
+The Quality of technical designs/codes/testing/Ticket analysis/resolution should be high as measured by Review - Minimum or Zero defects 
+Gain expertise on required Process, Method and Tools required for Role eg Agile, Remedy, CTD …
+All deliverables delivered on time 100% Schedule Adherence. 
+Independently create Technical Designs / provide solutions / process Improvements -  Work closely with end client business users to provide appropriate functional solutions and continually Identify better ways of task/process design/execution
+Develop and maintain high quality of communication - written (mails/documentation) and verbal (conference calls, presentation etc.)
+Maintain high quality of documentation - In form of design documents, tech specs, Test scripts/cases, Ticket resolution, RCA, Change Migration request etc.
+Develop Presentation skills - By way of presenting tech/functional specs, ticket resolution approach, test approach etc.
